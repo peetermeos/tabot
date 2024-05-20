aafcc72ee5a087b95b6cc9f5491285d3542f3526
Peeter Meos <peeter@gridio.io>
2024-05-20 23:36:41 +0300

    Adding matrices

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/websocket v1.5.1
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.3
+	gonum.org/v1/gonum v0.15.0
 )
 
 require (
--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -1,9 +1,25 @@
 package service
 
+import (
+	"fmt"
+	"gonum.org/v1/gonum/mat"
+)
+
 type Tick struct {
 	Symbol string
 	Bid    float64
 	BidQty float64
 	Ask    float64
 	AskQty float64
+}
+
+type TriangleBot struct {
+}
+
+func (t *TriangleBot) Run() {
+	// TODO: Just a placeholder for now
+	//   we will be constructing triangle legs via
+	//   matrix multiplication of exchange rates
+	zero := mat.NewDense(3, 5, nil)
+	fmt.Println(zero)
 }
