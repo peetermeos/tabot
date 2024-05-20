42f1c789a0ee54dfcb87d29edf9123d43857f42d
Peeter Meos <peeter@gridio.io>
2024-05-20 21:45:49 +0300

    Config boilerplate

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"os"
+
+	"github.com/peetermeos/tabot/config"
+	"github.com/sirupsen/logrus"
+)
+
 func main() {
+	tabotLogger := logrus.WithField("origin", "tabot")
+
+	_, err := config.Load()
+	if err != nil {
+		tabotLogger.WithError(err).Error("error loading config")
+
+		os.Exit(1)
+	}
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,51 @@
 package config
+
+import (
+	"os"
+	"reflect"
+
+	"github.com/pkg/errors"
+)
+
+type Config struct {
+	LogLevel  string `env:"LOGLEVEL"`
+	AWSRegion string `env:"AWS_REGION"`
+}
+
+var ErrFieldNotDefined = errors.New("environment variable name tag for field is not defined")
+
+func Load() (*Config, error) {
+	config := Config{
+		LogLevel:  "debug",
+		AWSRegion: "us-east-1",
+	}
+
+	typeOf := reflect.TypeOf(config)
+	valueOf := reflect.Indirect(reflect.ValueOf(&config))
+
+	// using reflection iterate over config struct fields
+	for i := 0; i < typeOf.NumField(); i++ {
+		// get field name
+		field := typeOf.Field(i)
+
+		// get field "env" tag value
+		tag, ok := field.Tag.Lookup("env")
+		if !ok {
+			return nil, errors.Wrap(ErrFieldNotDefined, field.Name)
+		}
+
+		// override default config field value with environment variable value if set
+		value, ok := os.LookupEnv(tag)
+		if ok {
+			if field.Type.Kind() == reflect.String {
+				valueOf.FieldByName(field.Name).SetString(value)
+			}
+
+			if field.Type.Kind() == reflect.Bool {
+				valueOf.FieldByName(field.Name).SetBool(true)
+			}
+		}
+	}
+
+	return &config, nil
+}
